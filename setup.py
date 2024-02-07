70c82105f08eb8cb2b7b6d49fb2552cc2ceeda53
Anders Lauridsen <ahll19@student.aau.dk>
2024-02-07 10:10:17 +0100

    Created a setup.py file. Added missing dependency in docker conatiner
    The pre-processing docker container was missing a cmake dependency, and I created a setup.py file to help  the container install the m2t module

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='m2t',
+    version='0.1',
+    packages=['m2t/models', 'm2t/dataset_utils', 'm2t/preprocessing'],
+    url='',
+    license='',
+    author='Josh Gardner, Peter Sobot',
+    author_email='',
+    description=''
+)
